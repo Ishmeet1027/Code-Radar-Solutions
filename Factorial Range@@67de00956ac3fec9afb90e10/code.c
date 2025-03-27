1839618c3e5842f8716402ef3341e7317c82135c
Ishmeet Kaur <ishmeetkaurpta@gmail.com>
2025-03-27 22:06:04 +0530

    Solution to Factorial Range

--- Factorial Range@@67de00956ac3fec9afb90e10/code.c
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -1,9 +1,29 @@
-#include<stdio.h>
-int factorialRange (int a, int b) {
-    int factorialRange =1;
-    for (int i=a;i<=b;i++) {
-        factorialRange = factorialRange*i;
-        return factorialRange ;
+#include <stdio.h>
+
+// Function to compute factorial of a number
+long long factorial(int n) {
+    long long fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
     }
-    printf("%d\n",factorialRange(i));
+    return fact;
 }
+
+// Function to print factorials of numbers in the range [a, b]
+void factorialRange(int a, int b) {
+    if (a > b || a < 0 || b < 0) {
+        printf("Invalid range\n");
+        return;
+    }
+
+    for (int i = a; i <= b; i++) {
+        printf("%d! = %lld\n", i, factorial(i));
+    }
+}
+
+// int main() {
+//     int start, end;
+//     scanf("%d %d", &start, &end);
+//     factorialRange(start, end);
+//     return 0;
+// }
