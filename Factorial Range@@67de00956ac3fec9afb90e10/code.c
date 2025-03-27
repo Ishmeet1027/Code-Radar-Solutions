f25a98feeedc2bcb9b474fe20de5c86328f148a2
Ishmeet Kaur <ishmeetkaurpta@gmail.com>
2025-03-27 21:58:55 +0530

    Solution to Factorial Range

--- Factorial Range@@67de00956ac3fec9afb90e10/code.c
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -2,7 +2,7 @@
 int factorialRange (int a, int b) {
     int factorialRange =1;
     for (int i=a;i<=b;i++) {
-        factorialRange = factorialRange*factorialRange(a-1);
+        factorialRange = factorialRange*factorialRange(b-1);
         return factorialRange ;
     }
 }
