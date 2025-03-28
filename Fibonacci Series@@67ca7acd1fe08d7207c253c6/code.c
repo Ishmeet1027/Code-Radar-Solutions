de7c07f77cd09dad0c136a9b1e37d806d76b3efc
Ishmeet Kaur <ishmeetkaurpta@gmail.com>
2025-03-28 21:05:25 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,11 @@
+int fibonacciSeries (int n) {
+    for (int i =0;i<=n;i++) {
+        int a,b;
+        a = 0;
+        b = 1;
+        int c = 1;
+        int fib = fib(n-1);
+    }
+    return 0;
+
+}
