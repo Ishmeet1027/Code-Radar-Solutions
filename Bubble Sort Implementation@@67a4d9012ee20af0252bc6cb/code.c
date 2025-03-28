271413e23a8a4d9c96bc087164502df6288e6c9c
Ishmeet Kaur <ishmeetkaurpta@gmail.com>
2025-03-28 14:27:03 +0530

    Solution to Bubble Sort Implementation

---
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int bubbleSort(arr, n) {
+    int max = arr[0];
+    for (int i=0;i<n;i++) {
+        if (arr[i]>max) {
+            max = array[i];
+        }
+    }
+    return 0;
+}
+int printArray(arr, n) {
+    printf("%d ",max);
+    return 0;
+}
